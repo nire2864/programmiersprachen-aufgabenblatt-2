9117cb52321729fa5acfff79663b0000034634f0
Luigi Portwich <luigi.vinzenz.portwich@uni-weimar.de>
2019-04-24 18:37:37 +0200

    Add Testcases freie Funktionen

--- source/tests.cpp
+++ source/tests.cpp
@@ -46,11 +46,71 @@ TEST_CASE("Vec2 operator /=" , "[Vec2]")
 {
   Vec2 first{1.0,2.0};
   float s = 2.0f;
+  float t = 0.0f;
   
   first /= s;
   REQUIRE(first.x == 0.5f);
   REQUIRE(first.y == 1.0f);
+  //Division by zero
+  first /= t;
+  //REQUIRE(first.x == 0.0f);
 }
+
+TEST_CASE("Freier operator+", "[Vec2]")
+{
+  Vec2 u{3.7f,4.2f};
+  Vec2 v{1.3f,2.8f};
+  
+  Vec2 result = u + v;
+  REQUIRE(result.x == 5.0f);
+  REQUIRE(result.y == 7.0f);
+}
+
+TEST_CASE("Freier operator-", "[Vec2]")
+{
+  Vec2 u{-1.3f,2.0f};
+  Vec2 v{2.3f,4.3f};
+  
+  Vec2 result = u - v;
+  REQUIRE(result.x == Approx(-3.6f));
+  REQUIRE(result.y == Approx(-2.3f));
+}
+
+TEST_CASE("Freier operator*(Vec2,float)", "[Vec2]")
+{
+  Vec2 v{2.4f,3.0f};
+  float s = 1.5f;
+
+  Vec2 result = v*s;
+  REQUIRE(result.x == Approx(3.6f));
+  REQUIRE(result.y == 4.5f);
+}
+
+TEST_CASE("Freier operator/", "[Vec2]")
+{
+  Vec2 v{3.5f,2.0f};
+  float s = 2;
+  float t = 0;
+
+  Vec2 result = v/s;
+  REQUIRE(result.x == 1.75f);
+  REQUIRE(result.y == 1.0f);
+
+  Vec2 result2 = v/t;
+  REQUIRE(result2.x == 0.0f);
+  REQUIRE(result2.y == -1.0f);
+}
+
+TEST_CASE("Freier operator*(float,Vec2)", "[Vec2]")
+{
+  Vec2 v{2.4f,3.0f};
+  float s = 1.5f;
+
+  Vec2 result = s*v;
+  REQUIRE(result.x == Approx(3.6f));
+  REQUIRE(result.y == 4.5f);
+}
+
 
 int main(int argc, char *argv[])
 {
--- source/vec2.cpp
+++ source/vec2.cpp
@@ -2,34 +2,42 @@
 #include "vec2.hpp"
 
 //Memberfunktionen
-Vec2& Vec2::operator +=(Vec2 const& v) 
+Vec2& Vec2::operator+=(Vec2 const& v) 
 {
 y += v.y;
 x += v.x;
 return *this;
 };
 
-Vec2& Vec2::operator -=(Vec2 const& v) 
+Vec2& Vec2::operator-=(Vec2 const& v) 
 {
     y -= v.y;
     x -= v.x;
     return *this;
 };
 
-Vec2& Vec2::operator *=(float s)
+Vec2& Vec2::operator*=(float s)
  {
      y *= s;
      x *= s;
      return *this;
  };
 
-Vec2& Vec2::operator /=(float s)
+Vec2& Vec2::operator/=(float s)
  {
+ if(s == 0) 
+    {
+    Vec2 result{0, 0};
+    std::cout << "Division by zero is undefined.\n";
+    return result;
+    }   
+else
+{
      y /= s;
      x /= s;
      return *this;
  };
-
+}
 //Freie Funktionen
 
 Vec2 operator+(Vec2 const& u, Vec2 const& v)
@@ -46,18 +54,18 @@ result -= v;
 return result;
 }
 
-Vec2 operator * (Vec2 const& v, float s)
+Vec2 operator*(Vec2 const& v, float s)
 {
 Vec2 result{v};
 result *= s;
 return result;
 }
 
-Vec2 operator / (Vec2 const& v, float s)
+Vec2 operator/(Vec2 const& v, float s)
 {
 if(s == 0) 
     {
-    Vec2 result{0, -1};
+    Vec2 result{0, 0};
     std::cout << "Division by zero is undefined.\n";
     return result;
     }   
@@ -69,7 +77,7 @@ else
     }
 }
 
-Vec2 operator * (float s, Vec2 const& v)
+Vec2 operator*(float s, Vec2 const& v)
 {
 Vec2 result{v};
 result *= s;
