5022fee5e84f3d3b562dedce4072dba4b4576f65
Luigi Portwich <luigi.vinzenz.portwich@uni-weimar.de>
2019-04-24 17:40:56 +0200

    Add Freie Funktionen Vektoren

--- source/vec2.cpp
+++ source/vec2.cpp
@@ -1,5 +1,7 @@
+#include <iostream>
 #include "vec2.hpp"
 
+//Memberfunktionen
 Vec2& Vec2::operator +=(Vec2 const& v) 
 {
 y += v.y;
@@ -27,3 +29,49 @@ Vec2& Vec2::operator /=(float s)
      x /= s;
      return *this;
  };
+
+//Freie Funktionen
+
+Vec2 operator+(Vec2 const& u, Vec2 const& v)
+{
+Vec2 result{u};
+result += v;
+return result;
+};
+
+Vec2 operator-(Vec2 const& u, Vec2 const& v)
+{
+Vec2 result{u};
+result -= v;
+return result;
+}
+
+Vec2 operator * (Vec2 const& v, float s)
+{
+Vec2 result{v};
+result *= s;
+return result;
+}
+
+Vec2 operator / (Vec2 const& v, float s)
+{
+if(s == 0) 
+    {
+    Vec2 result{0, -1};
+    std::cout << "Division by zero is undefined.\n";
+    return result;
+    }   
+else
+    {
+    Vec2 result{v};
+    result /= s;
+    return result;
+    }
+}
+
+Vec2 operator * (float s, Vec2 const& v)
+{
+Vec2 result{v};
+result *= s;
+return result;
+}
