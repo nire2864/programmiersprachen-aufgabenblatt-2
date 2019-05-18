f1947b6838a9c552b03c9a8e64aa1de852e0a798
Luigi Portwich <luigi.vinzenz.portwich@uni-weimar.de>
2019-05-18 16:56:03 +0200

    Add Aufgabe 2.7 in aufgabe_6.cpp

--- source/aufgabe_6.cpp
+++ source/aufgabe_6.cpp
@@ -1,6 +1,10 @@
 #include "circle.hpp"
 #include <iostream>
+#include <vector>
 #include <catch.hpp>
+
+
+#include <algorithm>
 
 int main()
 {
@@ -10,7 +14,24 @@ int main()
     Circle circle4{23.0f,{25.0f,30.0f},{0.0f,1.0f,0.0f},"Ferdinant"};
     Circle circle5{23.5f,{25.3f,30.9f},{0.34f,1.0f,0.0f},"Ferdinant"};
  
+    std::vector<Circle> sorted_circles;
+    sorted_circles.push_back(circle1);
+    sorted_circles.push_back(circle2);
+    sorted_circles.push_back(circle3);
+    sorted_circles.push_back(circle4);
+    sorted_circles.push_back(circle5);
+    std::sort(sorted_circles.begin(),sorted_circles.end());
+    //REQUIRE(std::is_sorted(sorted_circles.begin(),sorted_circles.end()));
+
+    std::sort(sorted_circles.begin(),sorted_circles.end(),
+    [](Circle const& first, Circle const& second) -> bool {
+        return (first > second);
+    });
     
-    
+    for(Circle circle : sorted_circles)
+    {
+        std::cout << circle << std::endl; 
+    }
+
     return 0;
 }
--- source/tests.cpp
+++ source/tests.cpp
@@ -316,6 +316,25 @@ TEST_CASE("operator==", "[Cirlce]")
     REQUIRE((circle1 == circle2)==false);
 }
 
+TEST_CASE("sorting circles")
+{
+    Circle circle1{23.0f,{25.0f,30.0f},{},"Hans"};
+    Circle circle2{20.0f,{15.0f,4.0f},{0.33f,0.22f,0.33f},"Peter"};
+    Circle circle3{3.0f,{10.0f,30.0f},{1.0f,0.0f,1.0f},"Otto"};
+    Circle circle4{23.0f,{25.0f,30.0f},{0.0f,1.0f,0.0f},"Ferdinant"};
+    Circle circle5{23.5f,{25.3f,30.9f},{0.34f,1.0f,0.0f},"Ferdinant"};
+ 
+    std::vector<Circle> sorted_circles;
+    sorted_circles.push_back(circle1);
+    sorted_circles.push_back(circle2);
+    sorted_circles.push_back(circle3);
+    sorted_circles.push_back(circle4);
+    sorted_circles.push_back(circle5);
+    std::sort(sorted_circles.begin(),sorted_circles.end());
+    REQUIRE(std::is_sorted(sorted_circles.begin(),sorted_circles.end()));
+
+}
+
 int main(int argc, char *argv[])
 {
   return Catch::Session().run(argc, argv);
