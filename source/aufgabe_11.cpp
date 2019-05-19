5446cd67f598f07fa059e462a28246ab2dd8ca15
Luigi Portwich <luigi.vinzenz.portwich@uni-weimar.de>
2019-05-19 14:25:21 +0200

    Add descriptions for Aufgabe 2.9

--- source/aufgabe_11.cpp
+++ source/aufgabe_11.cpp
@@ -44,6 +44,16 @@ TEST_CASE("filter alle vielfache ovn drei", "[erase]")
 
 
     REQUIRE(std::all_of(vector_of_random_ints.begin(), vector_of_random_ints.end(), is_multiple_of_3));
+}
+
+TEST_CASE("vector stellenweise addieren", "[Aufgabe12]")
+{
+    std::vector<int> v_1{1,2,3,4,5,6,7,8,9};
+    std::vector<int> v_2{9,8,7,6,5,4,3,2,1};
+    std::vector<int> v_3(9);
+
+    std::transform(v_1.begin(),v_1.end(),v_2.begin(),v_3.begin(),[](int i, int j){return i + j;});
+    REQUIRE(std::all_of(v_3.begin(),v_3.end() , [](int i){return i == 10;}));
 
 }
 
--- source/beispielprogramm.cpp
+++ source/beispielprogramm.cpp
@@ -7,6 +7,9 @@
 
 int main()
 {
+    /*
+        Fülle den Vector v_0 mit 10 Zufallszahlen
+    */
     std::vector<int> v_0(10);
 
     for (auto& v: v_0)
@@ -14,19 +17,36 @@ int main()
         v = std::rand();
     }
 
+    /*
+    Vector ausgeben (in ein ostream-objekt koperien und dann ausgeben)
+   */
     std::copy(std::cbegin(v_0), std::cend(v_0),
     std::ostream_iterator<int>(std::cout, "\n"));
 
+
+    /*
+    Übertragen von v_0 in die Listen l_0 und l_1
+   */
     std::list<int> l_0(std::cbegin(v_0), std::cend(v_0));
     std::list<int> l_1(std::cbegin(l_0), std::cend(l_0));
+    
+    /*
+    l_1 wird umgedreht und danach wie oben ausgegeben
+   */
     std::reverse(std::begin(l_1), std::end(l_1));
     std::copy(std::cbegin(l_1), std::cend(l_1),
     std::ostream_iterator<int>(std::cout, "\n"));
 
+    /*
+    l_1 wird sortiert und dananach ausgegeben
+   */
     l_1.sort();
     std::copy(l_1.cbegin(), l_1.cend(),
     std::ostream_iterator<int>(std::cout, "\n"));
 
+    /*
+    V_0 werden neue Zufallszahlen zugewiesen und dann ausgegeben
+   */
     std::generate(std::begin(v_0), std::end(v_0), std::rand);
     std::copy(v_0.crbegin(), v_0.crend(),
     std::ostream_iterator<int>(std::cout, "\n"));
